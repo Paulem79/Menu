1b1870c4b18919637e7d02bb32850ebcb0389ab5
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2022-06-17 23:44:24 +0200

    Version 0.0.2

--- utils/plugins/variable.py
+++ utils/plugins/variable.py
@@ -3,7 +3,7 @@ from colorama import Fore
 name = "Menu (Burger Edition)"
 author = "PauLem79"
 releaseurl = "https://github.com/Paulem79/Menu"
-THIS_VERSION = "0.0.1"
+THIS_VERSION = "0.0.2"
 
 y = Fore.YELLOW
 b = Fore.LIGHTBLUE_EX
